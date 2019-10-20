0689411925bc9c238328709b97f6c9bd19e6c08d
shohidul islam <theshohidul@gmail.com>
2019-10-20 23:28:29 +0600

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+# Python - Game - Hangman
+
+`Hangman` is a classic word game in which you must guess the secret word one letter at a time. This version is a simple console game, made by using python programming language for my learning purpose. My main focus of this project is to understand some basics of python programming language as a beginner.
+
+# Features
+  - You have to guess a word to save a man.
+  - Each correctly guessed letter is added to the word.
+  - Every wrong guessing will gradually drag the man to the death
+  - After all turns, if you can't guess the whole word, the man will die.
+
+
+# Screenshots
+
+
---
+++ hangman.py
@@ -0,0 +1,89 @@
+import random
+import math
+
+class Hangman:
+
+     # Hangman game
+
+    # hang stage and rope figure
+    hang_figure = []
+    hang_figure.append(' +----+')
+    hang_figure.append('      |')
+    hang_figure.append('      |')
+    hang_figure.append('      |')
+    hang_figure.append('      |')
+    hang_figure.append('      |')
+    hang_figure.append('=======')
+
+    #man figure
+    
+    man_figure = {}
+    man_figure[0] = [' |    |']
+
+    man_figure[1] = [' |    |',' 0    |']
+    man_figure[2] = [' |    |',' 0    |', ' |    |']
+
+    man_figure[3] = [' |    |','\\0    |', ' |    |']
+    man_figure[4] = [' |    |','\\0/   |', ' |    |']
+
+    man_figure[5] = [' |    |','\\0/   |', ' |    |', '/     |']
+    man_figure[6] = [' |    |','\\0/   |', ' |    |', '/ \\   |']
+
+    man_figure[7] = [' |    |',' 0/   |', '/|    |', '/ \\   |']
+    man_figure[8] = [' |    |',' 0    |', '/|\\   |', '/ \\   |']
+    man_figure[9] = [' |    |',' 0    |', '/|\\   |', '/ \\   |','DEAD  |']
+    
+    hangman = []
+    
+    def __init__(self):
+        i, j = 1, 0
+        self.hangman.append(self.hang_figure[:])
+        for ls in self.man_figure.values():
+            pic, j = self.hang_figure[:], 0
+            for m in ls:
+                pic[i + j] = m
+                j += 1
+            self.hangman.append(pic)
+
+
+    def print_hangman( self, index ):
+        for line in self.hangman[index]:
+            print(line)
+
+    def start (self):
+        print ("\nWelcome to hangman. Please save a life guessing correct word.\n")
+        # a list of words, selected a random word
+        word = random.choice(['great', 'courage', 'student'])
+        #remaining turns
+        turns = len(word)*2
+        #store user input characters
+        guess = []
+        k = 0
+
+        while ( turns >= 0 ) : 
+            if ( turns == 0 ): 
+                print ("Hey! you have killed a man!")
+                break
+
+            hint = [l if l in guess else "_" for l in word]
+            print ("\nGuess the word : ", end="")
+            print (" ".join(hint) )
+            print (f"Turns left : {turns}" )
+            
+            if word == ("".join(hint) ) :
+                print ("\nThanks for saving a life.")
+                break
+
+            else :
+                char = input ('\nEnter a character : ')
+                if ( char in word ) :
+                    guess.append(char)
+                else :
+                    turns -= 1
+                    k = 10 - math.floor( 10*(turns/(len(word)*2)))
+                    if  k == (len(self.hangman)-1) and turns == 1 :
+                        k -= 1
+            self.print_hangman(k)
+
+
+Hangman().start()
